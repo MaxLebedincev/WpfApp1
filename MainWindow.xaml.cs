ddf99ad8ea6812fcfeea671a59915671483a39a0
AISGOROD\mv.lebedincev <mv.lebedincev@aisgorod.ru>
2025-02-22 17:58:10 +0400

    Min game

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Numerics;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -10,23 +13,93 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private (double x, double y) Player { get; set; }
+        private const int SizeCell = 50;
+
+        private const string Pathlevel = "Levels\\";
+
+        private (int x, int y) Player { get; set; }
+        private (double x, double y) Border { get; set; }
+
+        private int[][] Map { get; set; } 
 
         public MainWindow()
         {
             InitializeComponent();
 
-            Player = (50, 50);
+            InitMap();
+            TickMap();
+        }
+
+        private void InitMap()
+        {
+            var str = File.ReadAllLines($"{Pathlevel}level_1.txt");
+
+            var startPointPlayer = str.First().Split().Select(int.Parse).ToArray();
+            Player = (startPointPlayer[0], startPointPlayer[1]);
+
+            Map = new int[str.Length - 1][];
+            var temp = 0;
+            foreach (var line in str.Skip(1))
+            {
+                Map[temp] = line.Split().Select(int.Parse).ToArray();
+                temp++;
+            }
+
+            Border = (Map.Length * SizeCell, Map.Length * SizeCell);
+        }
+
+        private void TickMap()
+        {
+            canvas.Children.Clear();
+
+            for (int i = 1; i < Map.Length + 1; i++)
+            {
+                for (int j = 1; j < Map.Length + 1; j++)
+                {
+                    canvas.Children.Add(CustomRectangle(i * SizeCell, j * SizeCell, SizeCell, SizeCell, Brushes.Black, Map[i - 1][j - 1]));
+                }
+            }
+
+            canvas.Children.Add(CustomRectangle(Player.x * SizeCell, Player.y * SizeCell, SizeCell, SizeCell, Brushes.Red, 2));
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            Player = (Player.x - 1, Player.y - 1);
+
+            Player = e.Key switch
+            {
+                Key.Right => (Map[Player.x + 1][Player.y] == 1 ? Player.x : Player.x + 1 , Player.y),
+                Key.Left => (Map[Player.x - 1][Player.y] == 1 ? Player.x : Player.x - 1, Player.y),
+                Key.Up => (Player.x, Map[Player.x][Player.y - 1] == 1 ? Player.y : Player.y - 1),
+                Key.Down => (Player.x, Map[Player.x][Player.y + 1] == 1 ? Player.y : Player.y + 1),
+                _ => (Player.x, Player.y),
+            };
+
+            if (Map[Player.x][Player.y] == 3)
+            {
+                MessageBox.Show("Вы победили!");
+            }
+
+            Player = (Player.x + 1, Player.y + 1);
 
             TickMap();
         }
 
-        private Polyline CustomRectangle(double x, double y, double height, double width, Brush solidColor, bool isUser = false)
+        private Polyline CustomRectangle(double x, double y, double height, double width, Brush solidColor, int color = 0)
         {
             Polyline rectangle = new Polyline();
 
             rectangle.Stroke = solidColor;
-            rectangle.Fill = isUser ? Brushes.Red : null;
+            rectangle.Fill = color switch
+            {
+                0 => Brushes.White,
+                1 => Brushes.Black,
+                2 => Brushes.Red,
+                3 => Brushes.Green,
+                _ => throw new NotImplementedException()
+            };
+
             rectangle.Points = new PointCollection(new List<Point>
             {
                 new Point(x, y),
@@ -37,35 +110,6 @@ namespace WpfApp1
             });
 
             return rectangle;
-        }
-
-        private void TickMap()
-        {
-            canvas.Children.Clear();
-
-            for (int i = 1; i < 4; i++)
-            {
-                for (int j = 1; j < 4; j++)
-                {
-                    canvas.Children.Add(CustomRectangle(i * 50, j * 50, 50, 50, Brushes.Black));
-                }
-            }
-
-            canvas.Children.Add(CustomRectangle(Player.x, Player.y, 50, 50, Brushes.Red, true));
-        }
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            Player = e.Key switch
-            {
-                Key.Right => (Player.x + 50, Player.y),
-                Key.Left => (Player.x - 50, Player.y),
-                Key.Up => (Player.x, Player.y - 50),
-                Key.Down => (Player.x, Player.y + 50),
-                _ => (Player.x, Player.y),
-            };
-
-            TickMap();
         }
     }
 }
